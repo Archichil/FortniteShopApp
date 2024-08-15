ea20173122c38e1cab85dfa5ec8001da54fb6263
Archichil <kuhockovolec@gmail.com>
2024-08-15 08:24:28 -0700

    Initial Commit

---
+++ FortniteShopApp/FortniteShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortniteShopAppApp.swift
+//  FortniteShopApp
+//
+//  Created by Archichil on 15.08.24.
+//
+
+import SwiftUI
+
+@main
+struct FortniteShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
