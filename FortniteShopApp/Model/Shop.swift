f1a0053c0fc6fe8ec5cae9930af75711f8024175
Archichil <kuhockovolec@gmail.com>
2024-08-19 11:30:11 -0700

    Shop model

---
+++ FortniteShopApp/Model/Shop.swift
@@ -0,0 +1,49 @@
+//
+//  Shop.swift
+//  FortniteShopApp
+//
+//  Created by Archichil on 15.08.24.
+//
+
+import Foundation
+
+struct Shop: Decodable {
+    let result: Bool
+    let fullShop: Bool
+    let lastUpdate: LastUpdate
+    let shop: [ShopItem]
+}
+
+struct LastUpdate: Decodable {
+    let date: String
+    let uid: String
+}
+
+struct ShopItem: Decodable {
+    let mainId: String
+    let displayName: String
+    let displayDescription: String
+    let displayType: String
+    let displayAssets: DisplayAssets
+    let price: Price
+    let rarity: Rarity
+}
+
+struct DisplayAssets: Decodable {
+    let url: String
+    let background: String
+    let fullBackground: String
+    
+    enum CodingKeys: String, CodingKey {
+        case url, background
+        case fullBackground = "full_background"
+    }
+}
+
+struct Price: Decodable {
+    let regularPrice, finalPrice, floorPrice: Int
+}
+
+struct Rarity: Decodable {
+    let id, name: String
+}
