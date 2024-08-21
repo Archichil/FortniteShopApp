51a6d717e53183ea18d8fe750a1815eb5150805c
Archichil <kuhockovolec@gmail.com>
2024-08-20 21:17:45 -0700

    Designed main ShopView

---
+++ FortniteShopApp/View/ShopView.swift
@@ -0,0 +1,38 @@
+//
+//  ShopView.swift
+//  FortniteShopApp
+//
+//  Created by Archichil on 19.08.24.
+//
+
+import SwiftUI
+
+struct ShopView: View {
+    
+    @ObservedObject private var viewModel = ShopViewModel()
+    
+    var body: some View {
+//        VStack {}
+        ScrollView {
+            ForEach(viewModel.sortedShop.sections) { section in
+                Section(header: 
+                            Text(section.sectionName)
+                                .font(.title2)
+                                .bold()) {
+                    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+                    
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(section.items, id: \.devName) { item in
+                            ShopItemView(item: item)
+                        }
+                    }
+                }
+                .padding(.top)
+            }
+        }
+    }
+}
+
+#Preview {
+    ShopView()
+}
